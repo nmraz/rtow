7295f8cc2e4a66a8e0fa96ce73dca8e80a3f3771
Noam Raz <noamraz8@gmail.com>
2021-03-26 01:20:29 +0200

    Create ray structure

--- src/math.rs
+++ src/math.rs
@@ -2,3 +2,15 @@ use nalgebra::{Unit, Vector3};
 
 pub type Vec3 = Vector3<f64>;
 pub type Unit3 = Unit<Vec3>;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Ray {
+    pub origin: Vec3,
+    pub dir: Unit3,
+}
+
+impl Ray {
+    pub fn at(&self, t: f64) -> Vec3 {
+        self.origin + t * self.dir.into_inner()
+    }
+}
