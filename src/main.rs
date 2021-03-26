16b80cfcf8d86c13551da38278c6a89e7218d84c
Noam Raz <noamraz8@gmail.com>
2021-03-26 03:12:17 +0300

    Shade sphere based on intersection normal

--- src/main.rs
+++ src/main.rs
@@ -48,20 +48,27 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn ray_color(ray: &Ray) -> Vec3 {
-    if hit_sphere(Vec3::new(0., 0., -1.), 0.5, ray) {
-        return Vec3::new(1., 0., 0.);
+    let center = Vec3::new(0., 0., -1.);
+
+    if let Some(t) = hit_sphere(center, 0.5, ray) {
+        let n = (ray.at(t) - center).normalize();
+        return 0.5 * (n + Vec3::new(1., 1., 1.));
     }
 
     let t = 0.5 * (ray.dir[1] + 1.);
     (1. - t) * Vec3::new(1., 1., 1.) + t * Vec3::new(0.5, 0.7, 1.)
 }
 
-fn hit_sphere(center: Vec3, radius: f64, ray: &Ray) -> bool {
+fn hit_sphere(center: Vec3, radius: f64, ray: &Ray) -> Option<f64> {
     let oc = ray.origin - center;
     let b = 2. * oc.dot(ray.dir.as_ref());
     let c = oc.norm_squared() - radius * radius;
 
     let discriminant = b * b - 4. * c;
 
-    discriminant > 0.
+    if discriminant >= 0. {
+        Some((-b - discriminant.sqrt()) / 2.)
+    } else {
+        None
+    }
 }
