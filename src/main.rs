a7b02aef2db4ccec52112b2a96e5f3c2b43f252b
Noam Raz <noamraz8@gmail.com>
2021-03-27 03:33:25 +0300

    Tweak scene

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::io::BufWriter;
 use std::sync::Arc;
 
 use geom::Sphere;
-use material::{Diffuse, Metal};
+use material::{Dielectric, Diffuse, Metal};
 use math::Vec3;
 use render::{Camera, RenderOptions};
 use scene::{Primitive, Scene};
@@ -18,14 +18,17 @@ mod scene;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ground_material = Arc::new(Diffuse::new(Vec3::new(0.5, 0.5, 0.5)));
-    let center_material = Arc::new(Diffuse::new(Vec3::new(1., 0.2, 0.2)));
-    let left_material = Arc::new(Metal::new(Vec3::new(0.8, 0.9, 0.8), 0.95));
-    let right_material = Arc::new(Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.4));
+    let pink_material = Arc::new(Diffuse::new(Vec3::new(1., 0.2, 0.2)));
+    let gold_material = Arc::new(Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.95));
+    let diamond_material = Arc::new(Dielectric::new(2.4));
 
     let scene = Scene::with_primitives(vec![
-        Primitive::new(Sphere::new(Vec3::new(0., 0., -1.), 0.5), center_material),
-        Primitive::new(Sphere::new(Vec3::new(-1., 0., -1.), 0.5), left_material),
-        Primitive::new(Sphere::new(Vec3::new(1., 0., -1.), 0.5), right_material),
+        Primitive::new(Sphere::new(Vec3::new(-0.5, 0., -1.), 0.5), pink_material),
+        Primitive::new(Sphere::new(Vec3::new(0.5, 0., -1.), 0.5), gold_material),
+        Primitive::new(
+            Sphere::new(Vec3::new(0., -0.15, -0.5), 0.1),
+            diamond_material,
+        ),
         Primitive::new(
             Sphere::new(Vec3::new(0., -100.5, -1.), 100.),
             ground_material,
@@ -35,8 +38,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let camera = Camera::new(960, 540);
 
     let opts = RenderOptions {
-        samples_per_pixel: 200,
-        max_depth: 10,
+        samples_per_pixel: 1000,
+        max_depth: 20,
     };
 
     let mut pixels = vec![Vec3::default(); (camera.pixel_width() * camera.pixel_height()) as usize];
